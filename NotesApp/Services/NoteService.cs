c1a7cdf5efc42a41f4b9c4a6644a19052c9e2005
GStrazdas <ged.strazdas@gmail.com>
2022-05-31 19:26:44 +0300

    Notes Added

---
+++ NotesApp/Services/NoteService.cs
@@ -0,0 +1,26 @@
+﻿using NotesApp.Model;
+using NotesApp.Repositories;
+
+namespace NotesApp.Services
+{
+    public class NoteService
+    {
+        private readonly NotesRepository _notesRepository;
+        private readonly UserService _userService;
+
+        public NoteService(NotesRepository notesRepository, UserService userService)
+        {
+            _notesRepository = notesRepository;
+            _userService = userService;
+        }
+
+        public void CreateNote(Note note)
+        {
+            var newNote = new Note();
+            newNote.Title = note.Title;
+            newNote.Content = note.Content;
+            newNote.NotesAppUserId = note.NotesAppUserId;
+            _notesRepository.CreateNote(newNote);
+        }
+    }
+}
